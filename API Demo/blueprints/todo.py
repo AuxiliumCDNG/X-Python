d4f7daf962aece1a5144225a908c354382f00e52
AuxiliumCDNG <47604750+AuxiliumCDNG@users.noreply.github.com>
2023-05-31 13:41:35 +0000

    todo api

--- API Demo/blueprints/todo.py
+++ API Demo/blueprints/todo.py
@@ -1,25 +1,68 @@
 from flask import Blueprint, request
-
 from db import factory, Todo
 
 todo = Blueprint("todo", __name__, url_prefix="/todo/")
 
+
 @todo.route("/add/")
 def todo_add():
     name = request.values.get("name")
+
     with factory() as session:
         session.add(Todo(aufgabe=name))
         session.commit()
 
-    return "success"
+    return "Aufgabe hinzugefügt"
 
-@todo.route("/set_fortschritt/<todo_id>/<fortschritt>/")
-def todo_set_fortschritt(todo_id, fortschritt):
-    # todo_id = request.values.get("id")
-    # fortschritt = request.values.get("fortschritt")
+
+@todo.route("/set_fortschritt/")
+def todo_set_fortschritt():
+    todo_id = request.values.get("id")
+    fortschritt = request.values.get("fortschritt")
+
     with factory() as session:
         todo_objekt = session.query(Todo).filter_by(id=todo_id).first()
         todo_objekt.fortschritt = fortschritt
         session.commit()
 
-    return "success"
+    return "Fortschritt verändert"
+
+
+@todo.route("/aufgabe_beenden/")
+def todo_aufgabe_beenden():
+    todo_id = request.values.get("id")
+
+    with factory() as session:
+        todo_objekt = session.query(Todo).filter_by(id=todo_id).first()
+        todo_objekt.fertig = True
+        session.commit()
+
+    return "Aufgabe beendet"
+
+
+@todo.route("/aufgabe_fortsetzen/")
+def todo_aufgabe_fortsetzen():
+    todo_id = request.values.get("id")
+
+    with factory() as session:
+        todo_objekt = session.query(Todo).filter_by(id=todo_id).first()
+        todo_objekt.fertig = False
+        session.commit()
+
+    return "Aufgabe neu gestartet"
+
+
+@todo.route("/uebersicht/")
+def todo_uebersicht_anzeigen():
+    with factory() as session:
+        if "nur_fertige" in request.values.keys():
+            todo_objekte = session.query(Todo).filter_by(fertig=1).all()
+        else:
+            todo_objekte = session.query(Todo).all()
+
+    ausgabe = []
+
+    for todo in todo_objekte:
+        ausgabe.append({"aufgabe": todo.aufgabe, "fortschritt": todo.fortschritt})
+
+    return ausgabe
